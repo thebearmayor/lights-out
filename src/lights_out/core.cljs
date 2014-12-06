a3690c845a0d79d9bb9efe905bed9921162eae76
Brad Peterson <bradley.peterson@bazaarvoice.com>
2014-12-06 16:08:00 -0600

    Simplify starting board-state

--- src/lights_out/core.cljs
+++ src/lights_out/core.cljs
@@ -21,17 +21,8 @@
   (reduce toggle-light state lights))
 
 (defonce board-state
-  (atom
-    (let [all-off (into {} (for [x (range 5) y (range 5)] [[x y] false]))]
-      (toggle-lights all-off [
-
-  [0 0]                   [0 4]
-        [1 1] [1 2] [1 3]
-        [2 1] [2 2] [2 3]
-        [3 1] [3 2] [3 3]
-  [4 0]                   [4 4]
-                              
-  ]))))
+  (atom {[4 3] false, [2 2] true, [0 0] true, [1 0] false, [2 3] true, [3 3] true, [1 1] true, [3 4] false, [4 2] false, [3 0] false, [4 1] false, [1 4] false, [1 3] true, [0 3] false, [2 4] false, [0 2] false, [2 0] false, [0 4] true, [3 1] true, [2 1] true, [4 4] true, [1 2] true, [3 2] true, [0 1] false, [4 0] true}))
+    
 
 (defn find-neighbors [[x y]]
   (for 
